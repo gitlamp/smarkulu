178cd28dd31cbf69f995c40a1ac0f4dda684ea92
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2017-09-17 13:15:07 +0430

    Add gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,24 @@
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  switch (stage) {
+    case 'develop':
+    
+    break;
+    
+    case 'develop-html':
+    
+    break;
+
+    case 'build-css':
+    
+    break;
+
+    case 'build-html':
+    
+    break;
+
+    case 'build-javascript':
+    
+    break;
+  }
+  return config
+}
