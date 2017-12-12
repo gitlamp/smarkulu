dec3f9430e186d7829dc42dfdaa43b05988efb0e
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2017-12-12 15:08:05 +0330

    Fixed logo switching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,10 @@ class Header extends React.Component {
         <div className="row header-row">
           {/* Logo */}
           <Link to={genLink(this.props.lang, '/')}>
-            <img className="col header-logo" src="/logos/logo-blue.svg" alt="blue logo"/>
+          {(this.props.lang !== 'fa')
+            ? <img className="col header-logo" src="/logos/logo-blue.svg" alt="blue logo"/>
+            : <img className="col header-logo" src="/logos/fa-logo-blue.svg" alt="blue logo"/>
+          }
           </Link>
           {/* Navigation */}
           <Menu menu={this.props.menu} url={this.props.url}/>
