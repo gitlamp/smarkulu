a7db1d8cfc3c7d43d4537d954a2c244b2324a740
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2018-02-12 14:20:01 +0330

    Change footer grid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import { Row, Col, getColumnProps } from 'react-flexbox-grid'
 import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
 
 class Footer extends React.Component {
@@ -23,7 +24,7 @@ class Footer extends React.Component {
       })
     })
 
-    return menu.map(list => {
+    return menu.map((list, i) => {
 
       // Check if each label has items
       const items = list.items
@@ -34,7 +35,7 @@ class Footer extends React.Component {
 
       // Render label and its items
       return (
-        <ul className="col footer-list" key={list.label}>
+        <ul className="footer-list" key={i}>
           <FormattedMessage id={list.label}>
             {(label) =>
               <li className="footer-list-item">{label}</li>
@@ -46,9 +47,9 @@ class Footer extends React.Component {
     })
   }
   getSocialItems = (social) => {
-    return social.map(item => {
+    return social.map((item, i) => {
       return (
-        <a className="footer-social-icon" href={item.link} key={item.link}>
+        <a className="footer-social-icon" href={item.link} key={i}>
           <i className={item.icon} aria-hidden="true"></i>
         </a>
       )
@@ -59,36 +60,40 @@ class Footer extends React.Component {
     const menuItems = this.getMenuItems(this.props.menu, langKey)
     const socialItems = this.getSocialItems(this.props.social)
     return (
-      <div className="container-fluid footer">
-        <div className="row footer-row">
+      <Row className="footer">
+        <Col xs={10} className="footer-row">
           {/* Render footer menu */}
+          <Row>
           {menuItems}
+          </Row>
           {/* Render socials */}
-          <div className="row w-100">
-            <div className="col-md-6 col-xl-6">
+          <Row>
+            <Col md={6} lg={6}>
               <FormattedMessage id="social.follow"/>
               <br/>
               {socialItems}
-            </div>
-            <div className="col-md-6 col-xl-6 footer-download">
+            </Col>
+            <Col md={6} lg={6} className="footer-download">
               <a className="footer-download-app" href="https://play.google.com/store/apps/details?id=com.taskulu.app" target="_blank">
                 <img src="/logos/download_google_play.svg" alt=""/>
               </a>
               <a className="footer-download-app" href="https://itunes.apple.com/us/app/taskulu/id1129696826?mt=8" target="_blank">
                 <img src="/logos/download_apple_store.svg" alt=""/>
               </a>
-            </div>
-          </div>
-        </div>
-        <div className="row footer-end">
-          <FormattedMessage id="footer.terms">
-          {(txt) =>
-          <a href="#">{txt}</a>
-          }
-          </FormattedMessage>
-          <span>Copyright	&copy; 2017 Taskulu</span>
-        </div>
-      </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={12} className="footer-end">
+          <Row center="xs" start="lg">
+            <FormattedMessage id="footer.terms">
+            {(txt) =>
+            <a href="#">{txt}</a>
+            }
+            </FormattedMessage>
+            <span>Copyright	&copy; 2017 Taskulu</span>
+          </Row>
+        </Col>
+      </Row>
     )
   }
 }
