3c2607730360f1bceb863119d308a809cbdc74fb
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2018-02-27 12:08:14 +0330

    Add about page styles

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import graphql from 'graphql'
-import { FormattedMessage } from 'react-intl'
-import { Grid, Row, Col } from 'react-flexbox-grid'
-import Helmet from 'react-helmet'
-import $ from 'jquery'
-import { TweenLite } from 'gsap'
+import { Row, Col } from 'react-flexbox-grid'
 
 import SEO from '../components/SEO'
-import Input from '../components/Input'
-import { Copy, CTA, Img } from '../components/Elements'
-import { TwoColumn, Above, Logos } from '../components/Partials'
+import { Copy } from '../components/Elements'
+import { Above } from '../components/Partials'
 
 class AboutPage extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
   }
-
   render() {
     const data = this.props.data
     const langKey = this.props.pathContext.langKey
@@ -26,26 +20,33 @@ class AboutPage extends React.Component {
           <div key={node.id}>
             <SEO pagePAth={langKey} title={node.header.title} generalDesc={node.header.desc} />
             <Above center="xs">
-              <Copy type="header" element="h1" child={node.body.header} />
+              <Col xs={10}>
+                <Copy type="header" element="h1" child={node.body.header} />
+              </Col>
             </Above>
-            <Row center="xs">
-              {node.body.team.map((item,i) => {
-                 return (
-                   <Col xs={6} sm={3} key={"member"+i} className="team-card">
-                     <div className="team-image">
-                       <img class="front" src={item.front} />
-                       <img class="back" src={item.back} />
-                       <Copy type="title" element="h3" child={item.name} />
-                       <Copy type="subtitle" element="h4" child={item.desc} />
-                       {item.twitter ? <a href={item.twitter}><i class="fa fa-twitter" aria-hidden="true"></i></a>:"" }
-                       {item.linkedin ? <a href={item.linkedin}><i class="fa fa-linkedin-square"></i></a>:""}
-                     </div>
-                   </Col>
-                 )
-              })}
+            <Row tagName="section" center="xs">
+              <Col xs={10}>
+                <Row center="xs">
+                  {node.body.team.map((item,i) => {
+                    const twitter = <a href={item.twitter} className="team-social-icon"><i className="fa fa-twitter twitter-highlight" aria-hidden="true"></i></a>
+                    const linkedin = <a href={item.linkedin} className="team-social-icon"><i className="fa fa-linkedin-square linkedin-highlight"></i></a>
+                    return (
+                      <Col xs={12} sm={3} key={i} className="team">
+                        <div className="team-card">
+                        <PersonsImg front={item.front} back={item.back}/>
+                        <Copy type="title" element="h3" child={item.name} className="team-name"/>
+                        <Copy type="subtitle" element="h4" child={item.jobTitle} className="team-job-title"/>
+                        {item.twitter ? twitter : null}
+                        {item.linkedin ? linkedin : null}
+                        </div>
+                      </Col>
+                    )
+                  })}
+                </Row>
+              </Col>
             </Row>
-            <Row center="xs">
-              <Col xs>
+            <Row tagName="section" center="xs">
+              <Col xs={10}>
                 <Copy type="header" element="h2" child={node.body.mission.header} />
                 <Copy type="content" element="p" child={node.body.mission.body} />
               </Col>
@@ -57,6 +58,38 @@ class AboutPage extends React.Component {
   }
 }
 
+class PersonsImg extends React.Component {
+  constructor(props) {
+    super(props)
+    this.MouseIn = this.MouseIn.bind(this)
+    this.MouseOut = this.MouseOut.bind(this)
+    this.state = {
+      faceSide: this.props.front
+    }
+  }
+  MouseIn() {
+    this.setState({
+      faceSide: this.props.back
+    })
+  }
+  MouseOut() {
+    this.setState({
+      faceSide: this.props.front
+    })
+  }
+  render() {
+    let img = null
+    if (this.props.front && this.props.back) {
+      img = <img src={this.state.faceSide} onMouseOver={this.MouseIn} onMouseOut={this.MouseOut} className="team-image"/>
+    } else {
+      img = "No Image"
+    }
+    return (
+      <div className="team-image">{img}</div>
+    )
+  }
+}
+
 export default AboutPage
 
 /**
@@ -64,7 +97,14 @@ export default AboutPage
  */
 export const pageQuery = graphql `
 query AboutPage {
-  allContentYaml(filter: {header: {lang: {eq: "en"}, slug: {eq: "/about"}}}) {
+  allContentYaml(
+    filter: {
+      header: {
+        lang: {eq: "en"}
+        slug: {eq: "/about"}
+      }
+    }
+  ){
     edges {
       node {
         id
@@ -76,7 +116,7 @@ query AboutPage {
           header
           team {
             name
-            desc
+            jobTitle
             twitter
             linkedin
             front
