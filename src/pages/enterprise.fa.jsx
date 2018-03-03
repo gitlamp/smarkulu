166329371ff4779e0f26957cfa4c9204586e88a9
sina <sina@taskulu.com>
2018-03-03 18:09:12 +0330

    Add test form to enterprise (fa) + styles

--- src/pages/enterprise.fa.jsx
+++ src/pages/enterprise.fa.jsx
@@ -16,18 +16,21 @@ const Enterprise = (props) => {
             pagePath={langKey}
             title={node.header.title}
             generalDesc={node.header.desc}/>
-            <Row className="double-divided">
-              <Col xs={12} sm={6} className="double-divided-col">
-                <Copy type="header" element="h1" className="double-divided-col-header" child={node.body.header}/>
-                <ul className="double-divided-col-body">
-                  {node.body.list.header}
-                  {node.body.list.items.map((item, i) =>
-                    <li key={i}>{item}</li>
-                  )}
-                </ul>
-              </Col>
-              <Col xs={12} sm={6} className="double-divided-col hasBackground"></Col>
-            </Row>
+          <Row className="double-divided">
+            <Col xs={12} sm={6} className="double-divided-col">
+              <Copy type="header" element="h1" className="double-divided-col-header" child={node.body.header}/>
+              <ul className="double-divided-col-body">
+                {node.body.list.header}
+                {node.body.list.items.map((item, i) =>
+                  <li key={i}>{item}</li>
+                )}
+              </ul>
+            </Col>
+            <Col xs={12} sm={6} className="double-divided-col hasBackground">
+              <div className="form-banner">{node.body.form.banner}</div>
+              <script type="text/javascript" src="//m.taskulu.com/form/generate.js?id=2"></script>
+            </Col>
+          </Row>
         </div>
       )}
     </div>
@@ -61,6 +64,9 @@ query EnterpriseFaPage {
           list {
             header
             items
+          }
+          form {
+            banner
           }
         }
       }
