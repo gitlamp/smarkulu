c51ab564dd358cad51885579bf7fc7fbb484d08a
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2018-04-21 17:24:13 +0430

    Remove unnecessary functions

--- src/components/functions.js
+++ src/components/functions.js
@@ -1,81 +1,6 @@
-import PropTypes from 'prop-types'
 /**
  * Custom functions
  */
-const ColumnSizeType = PropTypes.oneOfType([PropTypes.number, PropTypes.bool]);
-const ViewportSizeType = PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']);
-
-const colProps = {
-  xs: ColumnSizeType,
-  sm: ColumnSizeType,
-  md: ColumnSizeType,
-  lg: ColumnSizeType,
-  xl: ColumnSizeType,
-  xsOffset: PropTypes.number,
-  smOffset: PropTypes.number,
-  mdOffset: PropTypes.number,
-  lgOffset: PropTypes.number,
-  xlOffset: PropTypes.number,
-  first: ViewportSizeType,
-  last: ViewportSizeType,
-  className: PropTypes.string,
-  tagName: PropTypes.string,
-  children: PropTypes.node
-}
-const rowProps = {
-  reverse: PropTypes.bool,
-  start: ViewportSizeType,
-  center: ViewportSizeType,
-  end: ViewportSizeType,
-  top: ViewportSizeType,
-  middle: ViewportSizeType,
-  bottom: ViewportSizeType,
-  around: ViewportSizeType,
-  between: ViewportSizeType,
-  className: PropTypes.string,
-  tagName: PropTypes.string,
-  children: PropTypes.node,
-  column: PropTypes.bool
-}
-const gridProps = {
-  fluid: PropTypes.bool,
-  className: PropTypes.string,
-  tagName: PropTypes.string,
-  children: PropTypes.node
-}
-const createProps = (type, props) => {
-  const newProps = {}
-  var propTypes = []
-  switch(type) {
-    case 'col':
-      propTypes = colProps;
-      break;
-    case 'row':
-      propTypes = rowProps;
-      break;
-    case 'grid':
-      propTypes = gridProps;
-      break;
-  }
-  Object.keys(props)
-        .filter(key => (propTypes[key]))
-        .forEach(key => (newProps[key] = props[key]));
-  return newProps
-}
-
-const setlangKey = (langCode) => {
-  var langKey = ''
-  if(langCode) {
-    if(langCode !== 'IR') {
-      var langKey = 'en'
-      console.log('locale is en')
-    } else {
-      var langKey = 'fa'
-      console.log('locale is fa')
-    }
-  }
-  return langKey
-}
 
 const genLink = (lang, link) => {
   if (lang == 'en') {
@@ -97,4 +22,4 @@ const toPersianDigits = (v) => {
     return chars.join('')
 }
 
-export { createProps, setlangKey, genLink, toPersianDigits }
+export { genLink, toPersianDigits }
