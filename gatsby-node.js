e2e73b7702cea5774dad04a3a93b7d7e977826c8
Amirali Sariaslani <amirali.ie.1371@gmail.com>
2018-05-01 17:06:32 +0430

    Add source map in developement

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,25 +15,24 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
       loaders: ['modernizr-loader', 'json-loader']
     })
     config.merge({
+      output: {
+        publicPath: 'http://localhost:8000/'
+      },
       module: {
         loaders: [
           {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=./fonts/[name].[ext]'
-          },
-          {
             test: /\.(sass|scss)$/,
             exclude: [/\.useable\.scss$/, /\.rtl\.useable\.scss$/],
-            loaders: ['style-loader', 'css-loader', 'sass-loader']
+            loaders: ['style-loader', 'css-loader?sourceMap', 'sass-loader']
           },
           {
             test: /\.useable\.scss$/,
             exclude: /\.rtl\.useable\.scss$/,
-            loaders: ['style-loader/useable', 'css-loader', 'sass-loader']
+            loaders: ['style-loader/useable', 'css-loader?sourceMap', 'sass-loader']
           },
           {
-            test: /\.rtl\.useable\.scss$/,
-            loaders: ['style-loader/useable', 'rtlcss-loader', 'sass-loader']
+            test: /\.rtl\.useable.scss$/,
+            loaders: ['style-loader/useable', 'rtlcss-loader?sourceMap', 'sass-loader']
           }
         ]
       },
